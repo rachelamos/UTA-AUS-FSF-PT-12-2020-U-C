b39258ab92ad94d2b0f0c219390b4068ec6bbc5b
Rachel Amos <rachelamos35@gmail.com>
2021-01-16 09:59:07 -0600

    commiting for pulling upstream

--- 04-Web-APIs/02-Homework/Assets/script.js
+++ 04-Web-APIs/02-Homework/Assets/script.js
@@ -4,7 +4,8 @@ var timerEl = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var win = document.querySelector(".score");
 // var isWin = false;
-var timerCount 
+var timerCount = 60;
+var index = 0;
 
 
 function startTimer() {
@@ -28,11 +29,18 @@ function startTimer() {
       
 //   }
 
-
+// hide start button, then show questions and available answer choices
   startButton.addEventListener("click", function() {
       startTimer()
   });
 
+// as user selects answer, hide question/answer, show next question/answer; keeping score with each selection
+// function that shows first question + answer choices; index of questions
+// click event to record user's answers
+// grab answer, check to see if it's correct, if yes, alert via html banner and move on, if not, subtract time, alert via html banner, and move on
+// call next function with second question;
+// two total functions; need to figure out how to call multiple times without for loop
+// <button class="choice" id="A" onclick="checkAnswer('A')"></button>
 
 //   function resetQuiz() {
 //     // Resets win and loss counts
