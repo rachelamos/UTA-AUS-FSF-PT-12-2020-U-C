9fc4ee7090a9a0598885c934988779bf0af0d00b
Rachel Amos <rachelamos35@gmail.com>
2021-04-01 17:01:31 -0500

    yestreday's classwork

--- 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -4,4 +4,12 @@ module.exports = {
   },
   // TODO: Create a custom helper 'format_date' that takes in a timestamp,
   // adds five years to the date, and formats it as M/D/YYYY
+  format_date: (date) => {
+    newDate = new Date();
+    year = date.getFullYear();
+    month = date.getMonth();
+    day = date.getDate();
+    fulldate = new Date (month + 1, day, year + 5);
+    return fulldate;
+  }
 };
