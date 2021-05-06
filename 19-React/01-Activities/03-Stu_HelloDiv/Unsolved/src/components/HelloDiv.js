58d766b11d6de1ffb009abd1138d1095ec5aec0c
Rachel Amos <rachelamos35@gmail.com>
2021-05-06 13:59:03 -0500

    wed's work

--- 19-React/01-Activities/03-Stu_HelloDiv/Unsolved/src/components/HelloDiv.js
+++ 19-React/01-Activities/03-Stu_HelloDiv/Unsolved/src/components/HelloDiv.js
@@ -1,5 +1,16 @@
+import React from "react";
+
 function HelloDiv() {
-  return null;
+  return (
+    <div>
+      <h1>Hello! My name is Rachel</h1>
+      <ul>
+        <li>Jon</li>
+        <li>Arwen</li>
+        <li>Mama</li>
+      </ul>
+    </div>
+  );
 }
 
 export default HelloDiv;
